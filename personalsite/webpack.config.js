fa3c00709edad306aff08c3fda7471a2a89624e6
John Matthews <johnmatthews2634@gmail.com>
2018-09-27 10:57:23 -0700

    setup skeleton

---
+++ personalsite/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var webpack = require("webpack");
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/main.jsx",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
