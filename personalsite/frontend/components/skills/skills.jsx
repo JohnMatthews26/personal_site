b264f9f5e128f52c045383eb3350586c407edab2
John Matthews <johnmatthews2634@gmail.com>
2018-10-17 15:04:52 -0700

    styling changes

--- personalsite/frontend/components/skills/skills.jsx
+++ personalsite/frontend/components/skills/skills.jsx
@@ -19,7 +19,6 @@ class Skills extends Component {
   render(){
 
     return (
-
       <section className="skills-section">
         <div className="section-container">
           <div className="title-container">
@@ -28,41 +27,53 @@ class Skills extends Component {
             </h1>
           </div>
           <div className="skills-container">
-            <section id="languages">
-              <h2 className="skills-h2">Languages</h2>
-              <ul id="languages-ul" className="skills-ul">
-                <li id="language-li" className="skills-li">Ruby</li>
-                <li id="language-li" className="skills-li">Javascript</li>
-                <li id="language-li" className="skills-li">HTML</li>
-                <li id="language-li" className="skills-li">CSS</li>
-                <li id="language-li" className="skills-li">SQL</li>
-                <li id="language-li" className="skills-li">VBA</li>
-                <li id="language-li" className="skills-li">Python</li>
-              </ul>
+
+            <section id="languages" className="skills-column">
+              <div className="skills-border">
+                <h2 className="skills-h2">Languages</h2>
+                <ul id="languages-ul" className="skills-ul">
+                  <li id="language-li" className="skills-li">Ruby</li>
+                  <li id="language-li" className="skills-li">Javascript</li>
+                  <li id="language-li" className="skills-li">HTML</li>
+                  <li id="language-li" className="skills-li">CSS</li>
+                  <li id="language-li" className="skills-li">SQL</li>
+                  <li id="language-li" className="skills-li">VBA</li>
+                  <li id="language-li" className="skills-li">Python</li>
+                </ul>
+              </div>
             </section>
-            <section id="frameworks">
-              <h2 className="skills-h2">Frameworks</h2>
-              <ul id="frameworks-ul" className="skills-ul">
-                <li id="framework-li" className="skills-li">Ruby on Rails</li>
-                <li id="framework-li" className="skills-li">React Native</li>
-                <li id="framework-li" className="skills-li">Django</li>
-              </ul>
+
+            <section id="frameworks" className="skills-column">
+              <div className="skills-border">
+                <h2 className="skills-h2">Frameworks</h2>
+                <ul id="frameworks-ul" className="skills-ul">
+                  <li id="framework-li" className="skills-li">Ruby on Rails</li>
+                  <li id="framework-li" className="skills-li">React Native</li>
+                  <li id="framework-li" className="skills-li">Django</li>
+                </ul>
+              </div>
             </section>
-            <section id="libraries">
-              <h2 className="skills-h2">Libraries/Miscellaneous</h2>
-              <ul id="libraries-ul" className="skills-ul">
-                <li id="library-li" className="skills-li">React-Redux</li>
-                <li id="library-li" className="skills-li">JQuery</li>
-                <li id="library-li" className="skills-li">Node.js</li>
-                <li id="library-li" className="skills-li">Travis CI</li>
-                <li id="library-li" className="skills-li"></li>
-                <li id="library-li" className="skills-li"></li>
-                <li id="library-li" className="skills-li">CreateJS</li>
-              </ul>
+
+
+            <section id="libraries" className="skills-column">
+              <div className="skills-border">
+                <h2 className="skills-h2">Libraries/Miscellaneous</h2>
+                <ul id="libraries-ul" className="skills-ul">
+                  <li id="library-li" className="skills-li">React-Redux</li>
+                  <li id="library-li" className="skills-li">JQuery</li>
+                  <li id="library-li" className="skills-li">Node.js</li>
+                  <li id="library-li" className="skills-li">Travis CI</li>
+                  <li id="library-li" className="skills-li"></li>
+                  <li id="library-li" className="skills-li"></li>
+                  <li id="library-li" className="skills-li">CreateJS</li>
+                </ul>
+              </div>
             </section>
+
           </div>
         </div>
       </section>
+
     );
   }
 }
