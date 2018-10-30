9cdc907c673361779c246313a37f6a8e5f94bf07
John Matthews <johnmatthews2634@gmail.com>
2018-10-30 12:25:26 -0700

    about section and home stylining

--- personalsite/frontend/components/navbar/navbar.jsx
+++ personalsite/frontend/components/navbar/navbar.jsx
@@ -23,8 +23,8 @@ class Navbar extends Component {
       <div className="navbar-div">
         <div className="navbar-el" onClick={() => this.handleClick("splash-parent")}>home</div>
         <div className="navbar-el" onClick={() => this.handleClick("aboutme-parent")}>about</div>
-        <div className="navbar-el" onClick={() => this.handleClick("skills-parent")}>skills</div>
         <div className="navbar-el" onClick={() => this.handleClick("projects-parent")}>projects</div>
+        <div className="navbar-el" onClick={() => this.handleClick("skills-parent")}>skills</div>
       </div>
     );
   }
--- personalsite/frontend/components/splash/splash.jsx
+++ personalsite/frontend/components/splash/splash.jsx
@@ -43,13 +43,15 @@ class Splash extends Component {
       <section className="splash-section" id="splash-parent">
         <div className="section-container">
           <div className="title-container">
-            <h1 className="section-title">
+            <h1 className="section-title" id="splash-title">
               home
             </h1>
           </div>
           <div id="splash-content-container">
-            <div id="splash-blurb">Hi, I'm <span className="keywords">John Matthews</span>,
+            <div id="splash-blurb">Hi, I'm
+              <span className="keywords"> John Matthews</span>,
               and I'm looking for a full time role as a
+              <br></br>
               <span ref="roleGeneration" className="keywords"></span>
             </div>
           </div>
